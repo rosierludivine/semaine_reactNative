5a0f48ba094a62524a95f97e54f4653fda91df5b
rosierludivine <ludivine.rosier38@gmail.com>
2023-04-18 11:54:56 +0200

    installation de paper react native

--- AppNative1/App.js
+++ AppNative1/App.js
@@ -2,17 +2,20 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import {NavigationContainer} from '@react-navigation/native';
 import homeViews from './views/homeViews';
 import firstViews from './views/firstViews';
+import { Provider as PaperProvider } from 'react-native-paper';
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
+    <PaperProvider>
     <NavigationContainer>
       <Stack.Navigator>
         <Stack.Screen name="HomeApp" component={homeViews} options={{title: 'Home'}}/>
         <Stack.Screen name="firstViews" component={firstViews} options={{title: 'Second'}}/>
       </Stack.Navigator>
     </NavigationContainer>
+    </PaperProvider>
   );
 }
 

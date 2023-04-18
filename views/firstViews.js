a13e9e2485e1159002b4d5c151e11708273652f2
rosierludivine <ludivine.rosier38@gmail.com>
2023-04-18 14:49:01 +0200

    installation filesystem

---
+++ views/firstViews.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { Card, IconButton  } from 'react-native-paper';
+import { TextInput, Text } from 'react-native-paper';
+import { useState } from "react";
+
+function firstViews() {
+  const [text, setText] = useState("");
+
+  return (
+    <Card>
+      <TextInput label="texte"  value={text}   onChangeText={text => setText(text)} />
+      <IconButton icon="plus-circle" iconColor='#E4BE9E' size={70} onPress={() => console.log({text})}/>
+      <Text variant="displayLarge">{text}</Text>
+    </Card>
+  )
+}
+
+export default firstViews;
